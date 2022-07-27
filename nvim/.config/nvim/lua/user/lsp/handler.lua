2ab7ed94c1cf7e2cc6bb5299279536d82f225a7b
manuelcattelan <cattelanmanuel07@gmail.com>
2022-07-27 08:43:43 +0200

    add shortcut for lsp diagnostics

--- nvim/.config/nvim/lua/user/lsp/handler.lua
+++ nvim/.config/nvim/lua/user/lsp/handler.lua
@@ -60,11 +60,11 @@ local function lsp_keymaps(bufnr)
     local opts = { noremap = true, silent = true, buffer = bufnr }
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
     vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.hover, opts)
     vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
     vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
     vim.keymap.set('n', '<leader>f', vim.lsp.buf.formatting, opts)
 end
 
