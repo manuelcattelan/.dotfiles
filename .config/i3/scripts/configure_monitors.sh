2891d0727946b197067be1d77011ad61d53d1eef
Manuel Cattelan <cattelanmanuel01@gmail.com>
2025-03-14 08:54:50 +0100

    feat(i3): add initial config

---
+++ .config/i3/scripts/configure_monitors.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# Function to configure connected monitors
+configure_monitors() {
+    # Get the list of connected monitors
+    monitors_count=$(xrandr --query | grep " connected" | wc -l)
+
+   if [ $monitors_count -eq 2 ]; then
+        # Check if the external monitor is connected
+        if xrandr --query | grep -q "DP-1 connected"; then
+            xrandr --output eDP-1 --auto --output DP-1 --auto --right-of eDP-1
+        fi
+    else
+        # Enable the laptop's monitor if no external monitors are connected
+        eval xrandr --output eDP-1 --output DP-1 --off
+    fi
+}
+
+# Set the correct configuration for all connected monitors
+configure_monitors
+
+# Function to monitor udev events and reconfigure monitors when changes occur
+configure_udev() {
+    udevadm monitor --subsystem-match=drm --udev | while read -r line; do
+        if [[ $line == *"change"* ]] && [[ $line == *"drm"* ]]; then
+            # Sleep for 1 second to allow the monitors' configuration to be updated
+            sleep 1
+
+            # Set the correct configuration for all connected monitors
+            configure_monitors
+            # Set the wallpaper on all connected monitors
+            source $XDG_CONFIG_HOME/i3/scripts/configure_background.sh
+        fi
+    done
+}
+
+# Monitor udev events in the background
+(configure_udev) &
+
+# Detach the process from the script so that monitoring will continue running
+# when the script exits.
+disown $!
---
+++ .scripts/tweak_sink.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+# Function to display script usage
+echo_script_usage() {
+    echo "Usage: $(basename $0) <offset/toggle>"
+    echo "Increase/decrease sink volume by a percentage offset N or toggle mute."
+    echo
+    echo "offset:"
+    echo "  +N%  Increases sink volume by N% from current value, where N is any integer value."
+    echo "  -N%  Decreases sink volume by N% from current value, where N is any integer value."
+    echo
+    echo "toggle:"
+    echo "  toggle (when unmuted)  Mute sink volume"
+    echo "  toggle (when muted)    Unmute sink volume"
+    echo
+    echo "Examples:"
+    echo "  $(basename $0) +10%"
+    echo "  $(basename $0) -10%"
+    echo "  $(basename $0) toggle"
+
+    exit 1
+}
+
+# Adjust the sink volume or toggle mute based on the provided argument
+if [ -n $1 ] && ([ $1 == "toggle" ] || [[ $1 =~ ^[+-][0-9]+%$ ]]); then
+    if [ $1 == "toggle" ]; then
+        # Toggle the sink mute state
+        pactl set-sink-mute @DEFAULT_SINK@ toggle
+
+        if echo $(pactl get-sink-mute @DEFAULT_SINK@) | grep -q "Mute: yes"; then
+            sink_volume_status="muted"
+        else
+            sink_volume_status="unmuted"
+        fi
+
+        dunstify "Volume tweaked!" "Volume is now $sink_volume_status." \
+            -u low -h string:x-dunst-stack-tag:volume
+    else
+        # Adjust the sink volume
+        pactl set-sink-volume @DEFAULT_SINK@ $1
+
+        sink_volume_value=$(pactl get-sink-volume @DEFAULT_SINK@ \
+            | grep -oP '\d+(?=%)' | head -n 1)
+        if echo $(pactl get-sink-mute @DEFAULT_SINK@) | grep -q "Mute: yes"; then
+            sink_volume_status="muted"
+        else
+            sink_volume_status="unmuted"
+        fi
+
+        if [ $sink_volume_status == "muted" ] && [ $sink_volume_value -gt 0 ]; then
+            # Unmute the sink if the volume is non-zero
+            pactl set-sink-mute @DEFAULT_SINK@ false
+            # We do not need to explicitly set the sink volume since by
+            # toggling `mute` to false, pactl already sets it for us.
+            # pactl set-sink-volume @DEFAULT_SINK@ $1
+
+            dunstify "Volume tweaked!" "Volume is now unmuted at ~$sink_volume_value%." \
+                -u low -h string:x-dunst-stack-tag:volume
+        elif [ $sink_volume_value -eq 0 ]; then
+            # Mute the sink if the volume is zero
+            pactl set-sink-mute @DEFAULT_SINK@ true
+
+            dunstify "Volume tweaked!" "Volume is now muted." \
+                -u low -h string:x-dunst-stack-tag:volume
+        else
+            dunstify "Volume tweaked!" "Volume is now at ~$sink_volume_value%." \
+                -u low -h string:x-dunst-stack-tag:volume
+        fi
+    fi
+else
+    echo_script_usage
+fi
