7eb26bbad619f0e524388c20db16d32e90e5073d
Manuel Cattelan <cattelanmanuel01@gmail.com>
2025-05-25 17:44:26 +0200

    feat(nvim): add formatting rules for javascript and typescript

--- .config/nvim/lua/mcattelan/plugins/formatting.lua
+++ .config/nvim/lua/mcattelan/plugins/formatting.lua
@@ -1,7 +1,14 @@
 return {
   "stevearc/conform.nvim",
   opts = {
-    formatters_by_ft = { lua = { "stylua" }, c = { "clang-format" }, sh = { "beautysh" }, zsh = { "beautysh" } },
+    formatters_by_ft = {
+      lua = { "stylua" },
+      c = { "clang-format" },
+      sh = { "beautysh" },
+      zsh = { "beautysh" },
+      javascript = { "prettierd" },
+      typescript = { "prettierd" },
+    },
     format_on_save = function(bufnr)
       if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
         return
