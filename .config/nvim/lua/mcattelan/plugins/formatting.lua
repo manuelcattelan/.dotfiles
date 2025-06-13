03270c8005c7e7e83c99b27151872e97550fb41a
Manuel Cattelan <cattelanmanuel01@gmail.com>
2025-06-13 08:32:14 +0200

    feat(nvim): add formatting rules for python

--- .config/nvim/lua/mcattelan/plugins/formatting.lua
+++ .config/nvim/lua/mcattelan/plugins/formatting.lua
@@ -8,6 +8,7 @@ return {
       zsh = { "beautysh" },
       javascript = { "prettierd" },
       typescript = { "prettierd" },
+      python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
     },
     format_on_save = function(bufnr)
       if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
