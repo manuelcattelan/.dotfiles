0cc51c4296e7e4b715ca0764d584e223cba7fbf9
Manuel Cattelan <cattelanmanuel01@gmail.com>
2025-11-09 11:05:59 +0100

    feat: add js formatting configuration

--- .config/nvim/lua/mcattelan/plugins/formatting.lua
+++ .config/nvim/lua/mcattelan/plugins/formatting.lua
@@ -2,6 +2,7 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
+      javascript = { "prettier" },
       typescript = { "prettier" },
       typescriptreact = { "prettier" },
       python = { "black" }
